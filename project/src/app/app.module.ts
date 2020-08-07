c1c343fda9df3e2ab68ccc291b6f71713d427d66
sensationull <andrewjohnso17@gmail.com>
2020-08-06 20:35:31 -0700

    added notes about angular breaking changes for imports

--- project/src/app/app.module.ts
+++ project/src/app/app.module.ts
@@ -7,7 +7,12 @@ import {HttpClientModule} from '@angular/common/http';
 import { HomeComponent } from './home/home.component';
 import { AboutComponent } from './about/about.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-
+import {MatToolbarModule} from '@angular/material/toolbar';
+import { MatIconModule} from '@angular/material/icon';
+import { MatCardModule } from '@angular/material/card';
+import { MatButtonModule } from '@angular/material/button';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
+/* Angular updated to a breaking change that forces you to grab each component from it's package */
 @NgModule({
   declarations: [
     AppComponent,

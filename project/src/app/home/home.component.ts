8e4ce80f9a1073347288fb8ead48aa2c836ac0ba
sensationull <andrewjohnso17@gmail.com>
2020-08-08 11:58:05 -0700

    working on initializing app with faked data

--- project/src/app/home/home.component.ts
+++ project/src/app/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ApiService } from '../api.service';
 
 @Component({
   selector: 'app-home',
@@ -6,10 +7,14 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
+  products = [];
+  constructor(private apiService: ApiService) { }
 
-  constructor() { }
-
-  ngOnInit(): void {
+  ngOnInit() {
+    this.apiService.get().subscribe((data: any[]) => {
+      console.log('~This is the data~',data);
+      this.products = data;
+    })
   }
 
 }
